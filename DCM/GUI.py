21b45e972f18c0ebd627711cdffa702861b80f85
Samarth Mehta <mehtas30@mcmaster.ca>
2022-10-13 19:38:37 -0400

    gui done

--- DCM/GUI.py
+++ DCM/GUI.py
@@ -1,23 +1,221 @@
-#=====================================================Trial Testing===============================================================
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-import sys
+from cgitb import text
+import tkinter as tk
+from tkinter import messagebox
+from options import *
+BGCOLOR = '#800000'
 
 
-def main():
-    app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(200, 200, 300, 300)
-    win.setWindowTitle("My first window!")
+class gui (tk.Tk):  # tk.TK is root
+    # controller for pages
+    # *args and **kwargs can receive multiple parameters kwargs is for named values
+    def __init__(self, *args, **kwargs):
+        # init for tkinter functionality (superclass)
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame()  # container for frames
+        container.pack(side='top', fill='both', expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        self.pageInfo = {}  # empty dictionary for page name and object
+        # signupP, deleteP]  # array of class objects
+        for page in (welcomeP, loginP, signupP, deleteP, afterLogin):
+            pageName = page.__name__  # magic  method to get object name -pythons cool
+            # parent as container for frame, with self as controller to use function in gui
+            frame = page(parent=container, controller=self)
+            frame.grid(row=0, column=0, sticky='nesw')
+            # pagename dictionary with frame objects
+            self.pageInfo[pageName] = frame
+        self.dispFrame('welcomeP')  # show welcome first
 
-    label = QLabel(win)
-    label.setText("my first label")
-    label.move(100, 50)
-
-    win.show()
-    sys.exit(app.exec_())
+    def dispFrame(self, pageName):  # display frame based on name
+        page = self.pageInfo[pageName]
+        page.tkraise()
 
 
-main()
+class welcomeP(tk.Frame):  # Frame is parent
+    def __init__(self, parent, controller):  # init object as specified in gui
+        # init frame to get tkinter frame properties
+        tk.Frame.__init__(self, parent, bg=BGCOLOR)
+        self.controller = controller
+        self.controller.title('Heart Pacer 26')
+        # welcome label
+        title = tk.Label(self, text='Welcome', fg='#F2BA49', bg=BGCOLOR,
+                         justify='center', font="default, 25")
+        # title.place(x=400, y=100, anchor=CENTER)
+        title.pack(pady=10, padx=300)
+        # button for option select on welcome screen
+        loginButt = tk.Button(self, text="Login",
+                              width=20, height=2, command=lambda: controller.dispFrame("loginP"))
+        signButt = tk.Button(self, text="Signup",
+                             width=20, height=2, command=lambda: controller.dispFrame("signupP"))
+        deleteButt = tk.Button(
+            self, text="Delete", width=20, height=2, command=lambda: controller.dispFrame("deleteP"))
+        loginButt.pack(pady=5)
+        signButt.pack(pady=5)
+        deleteButt.pack(pady=5)
 
-#=====================================================Trial Testing===============================================================
+
+class loginP(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent, bg=BGCOLOR)
+        self.controller = controller
+
+        def clearBox():
+            user.delete(0, tk.END)
+            password.delete(0, tk.END)
+
+        def loginPressed(user, password):
+            clearBox()
+            loginInfo = login(user, password)
+            logCode = loginInfo[0]
+            logMsg = loginInfo[1]
+            messagebox.showinfo(message=logMsg)
+            if (logCode == 2):
+                controller.dispFrame("afterLogin")
+            elif (logCode == 1):
+                controller.dispFrame("welcomeP")
+            else:
+                pass
+        # Title label
+        title = tk.Label(self, text='Login', fg='#F2BA49', bg=BGCOLOR,
+                         justify='center', font="default, 25")
+        title.grid(row=0, column=1, pady=10, padx=200)
+        # button for option select on welcome screen
+        welcomeButt = tk.Button(self, text="To Main",
+                                width=5, height=2, command=lambda: controller.dispFrame("welcomeP"))
+        enterUser = tk.StringVar()
+        enterPass = tk.StringVar()
+        user = tk.Entry(self, bg="#FFFF9F", fg=BGCOLOR,
+                        textvariable=enterUser, width=20)
+        password = tk.Entry(self, bg="#FFFF9F", fg=BGCOLOR,
+                            textvariable=enterPass, width=20)
+        userIns = tk.Label(self, text='Username:', fg='#F2BA49', bg=BGCOLOR,
+                           justify='center', font="default, 25")
+        passIns = tk.Label(self, text='Password:', fg='#F2BA49', bg=BGCOLOR,
+                           justify='center', font="default, 25")
+        submit = tk.Button(self, text="Login",
+                           width=5, height=2, command=lambda: loginPressed(enterUser.get(), enterPass.get()))
+        welcomeButt.grid(row=0, column=0, pady=5)
+        user.grid(row=2, column=1, pady=5)
+        password.grid(row=3, column=1, pady=10)
+        userIns.grid(row=2, column=0)
+        passIns.grid(row=3, column=0)
+        submit.grid(row=4, column=1)
+
+
+class signupP(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent, bg=BGCOLOR)
+
+        self.controller = controller
+        # submit control
+
+        def clearBox():
+            user.delete(0, tk.END)
+            password.delete(0, tk.END)
+            passwordTwo.delete(0, tk.END)
+
+        def signupcheck(user, pw, pwconf):
+            clearBox()
+            signupRecieved = signup(user, pw, pwconf)
+            checkCode = signupRecieved[0]
+            messageInfo = signupRecieved[1]
+            messagebox.showinfo(message=messageInfo)
+            if (checkCode == 2):
+                controller.dispFrame("loginP")
+            elif (checkCode == 1):
+                pass
+            else:
+                controller.dispFrame("welcomeP")
+        # Title label
+        title = tk.Label(self, text='Signup', fg='#F2BA49', bg=BGCOLOR,
+                         justify='center', font="default, 25")
+        title.grid(row=0, column=1, pady=10, padx=200)
+        # buttons and entries
+        welcomeButt = tk.Button(self, text="To Main",
+                                width=5, height=2, command=lambda: controller.dispFrame("welcomeP"))
+        enterUser = tk.StringVar()
+        enterPass = tk.StringVar()
+        enterPassTwo = tk.StringVar()
+        user = tk.Entry(self, bg="#FFFF9F", fg=BGCOLOR,
+                        textvariable=enterUser, width=20)
+        password = tk.Entry(self, bg="#FFFF9F", fg=BGCOLOR,
+                            textvariable=enterPass, width=20)
+        passwordTwo = tk.Entry(self, bg="#FFFF9F", fg=BGCOLOR,
+                               textvariable=enterPassTwo, width=20)
+        submit = tk.Button(self, text="Submit",
+                           width=5, height=2, command=lambda: signupcheck(enterUser.get(), enterPass.get(), enterPassTwo.get()))
+        # labels
+        userIns = tk.Label(self, text='Username:', fg='#F2BA49', bg=BGCOLOR,
+                           justify='center', font="default, 25")
+        passIns = tk.Label(self, text='Password:', fg='#F2BA49', bg=BGCOLOR,
+                           justify='center', font="default, 25")
+        passInsTwo = tk.Label(self, text='Password:', fg='#F2BA49', bg=BGCOLOR,
+                              justify='center', font="default, 25")
+        # placement
+        welcomeButt.grid(row=0, column=0, pady=5)
+        user.grid(row=2, column=1, pady=5)
+        password.grid(row=3, column=1, pady=10)
+        passwordTwo.grid(row=4, column=1, pady=10)
+        userIns.grid(row=2, column=0)
+        passIns.grid(row=3, column=0)
+        passInsTwo.grid(row=4, column=0)
+        submit.grid(row=5, column=1)
+
+
+class deleteP(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent, bg=BGCOLOR)
+        self.controller = controller
+        # submit control
+
+        def clearBox():
+            user.delete(0, tk.END)
+            password.delete(0, tk.END)
+
+        def deleteUser(user, pw):
+            clearBox()
+            deleteMsg = delete(user, pw)
+            messagebox.showinfo(message=deleteMsg)
+            controller.dispFrame("welcomeP")
+        # Title label
+        title = tk.Label(self, text='Delete', fg='#F2BA49', bg=BGCOLOR,
+                         justify='center', font="default, 25")
+        title.grid(row=0, column=1, pady=10, padx=200)
+        # buttons and entries
+        welcomeButt = tk.Button(self, text="To Main",
+                                width=5, height=2, command=lambda: controller.dispFrame("welcomeP"))
+        enterUser = tk.StringVar()
+        enterPass = tk.StringVar()
+        user = tk.Entry(self, bg="#FFFF9F", fg=BGCOLOR,
+                        textvariable=enterUser, width=20)
+        password = tk.Entry(self, bg="#FFFF9F", fg=BGCOLOR,
+                            textvariable=enterPass, width=20)
+        submit = tk.Button(self, text="Submit",
+                           width=5, height=2, command=lambda: deleteUser(enterUser.get(), enterPass.get()))
+        # labels
+        userIns = tk.Label(self, text='Username:', fg='#F2BA49', bg=BGCOLOR,
+                           justify='center', font="default, 25")
+        passIns = tk.Label(self, text='Password:', fg='#F2BA49', bg=BGCOLOR,
+                           justify='center', font="default, 25")
+        # placement
+        welcomeButt.grid(row=0, column=0, pady=5)
+        user.grid(row=2, column=1, pady=5)
+        password.grid(row=3, column=1, pady=10)
+        userIns.grid(row=2, column=0)
+        passIns.grid(row=3, column=0)
+        submit.grid(row=5, column=1)
+
+
+class afterLogin(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent, bg=BGCOLOR)
+        self.controller = controller
+        # Title label
+        title = tk.Label(self, text='login success', fg='#F2BA49', bg=BGCOLOR,
+                         justify='center', font="default, 25")
+        title.pack()
+
+
+if __name__ == "__main__":
+    app = gui()
+    app.mainloop()
---
+++ DCM/options.py
@@ -0,0 +1,166 @@
+def counter():
+    database = open("Database", "r")  # open and read the database text file
+    counts = 0  # create a variable counts and set the intial value to 0
+    # create a variable line and set it to read the lines in the database text file
+    line = database.readlines()
+    for i in range(len(line)):  # for loop for the length of line
+        if line == "":  # if line is equal to an empty string
+            break  # break the for loop
+        counts += 1  # add 1 to the variable counts
+    database.close()  # close the database
+    return counts  # return the variable counts
+
+
+def signup(username, password, confirm_password):
+    database = open("Database", "r")  # open and read the database text file
+    count = counter()  # create a variable count and set it equal to counter ()
+    if count == 10:  # if the count is equal to 10
+        # let the user know the maximum amount of accounts have been reached (10)
+        print("Maximum accounts reached, please login into a existing account or delete an account")
+        return 0, "Maximum accounts reached, please login into a existing account or delete an account"
+        # select()  # give the user another attempt to choose what to do either signup, login or delete
+    else:  # if there is less than 10 users created let the user signup and create a new account
+        # create a username for the new account
+        # username = input("Create Username:")
+        # # create a password for the new account
+        # password = input("Create Password:")
+        # # double checks the password, confirms the password
+        # confirm_password = input("Confirm Password")
+        x = []  # create an empty array x
+        y = []  # create an empty array y
+        for i in database:  # for loop for every i in database
+            # splits the inputs for username and password by a comma
+            if (i == ""):
+                break
+            else:
+                a, b = i.split(", ")
+                b = b.strip()  # take out all the extra space inputs
+                x.append(a)  # add the string into the array
+                y.append(b)  # add the string into the array
+        if password != confirm_password:  # if the passowrd doesn't equal confirm password
+            # tell the user to try again as the passwords entered don't match each other
+            print("Passwords don't match, please try again")
+            return 1, "Passwords don't match, please try again"
+        else:  # if the passwords match then
+            if len(password) < 4:  # make sure the password is longer than 4 characters
+                # if the password is not longer than 4 charecters then tell the user the passwords to short
+                print("Password to short, please have more than 4 characters")
+                # signup()  # give the user another attempt to signup
+                return 1, "Password to short, please have more than 4 characters"
+            elif username in x:  # if the username is already in the array x
+                # tell the user that the username already exists in the database
+                print("Username already exists, please sign in or create a new username")
+                # select()  # give the user another attempt to choose what to do either signup, login or delete
+                return 0, "Username already exists, please sign in or create a new username"
+            elif username == password:  # if the password and username are the same
+                # tell the user that the password and username can't be the same
+                print("Password and Username can not be the same")
+                # signup()  # give the user another attempt to signup
+                return 1, "Password and Username can not be the same"
+            else:  # if everything passes then
+                # open up the databse text file
+                database = open("Database", "a")
+                # write the new username and passwords created
+                database.write(username + ", " + password + "\n")
+                database.close()  # close the database
+                # tell the user that the new account is succesfully created
+                print("Successfully created an account for",
+                      username, "please login")
+                # login()  # give the user a chance to login
+                return 2, "Successfully created an account! please login"
+
+
+def login(username, password):
+    returnArr = []
+    database = open("Database", "r")  # open and read the database text file
+    # if the number of chartecters entered for username or password is not less then 1 then
+    if not len(password or username) < 1:
+        x = []  # create an empty array x
+        y = []  # create an empty array y
+        for i in database:  # for loop for every i in database
+            # splits the inputs for username and password by a comma
+            a, b = i.split(", ")
+            b = b.strip()  # take out all the extra space inputs
+            x.append(a)  # add the string into the array
+            y.append(b)  # add the string into the array
+            # create a new variable called data and pass the arrays
+            data = dict(zip(x, y))
+        try:
+            if data[username]:  # if username is in data
+                try:
+                    # check if the password and the username are correct if they are then
+                    if password == data[username]:
+                        # let the user know that they've succesfully logged in
+                       # print("Login successful welcome", username)
+                        returnArr = [2, "Login successful welcome"]
+                    else:  # if the password and username is not correct then
+                        # let the user know that the password entered or the username is inccorect and tell them to try again
+                        # print("Password or username is incorrect, please try again")
+                        # login()  # run login
+                        returnArr = [
+                            0, "Password or username is incorrect, please try again"]
+                except:
+                    # let the user know that the password entered or the username is inccorect and tell them to try again
+                    # print("Password or username is incorrect, please try again")
+                    # login()  # run login
+                    returnArr = [
+                        0, "Password or username is incorrect, please try again"]
+            else:  # if the username or password doesn't exist then
+                # let the user know that the username or password does not existm and they shouuld either create an account or sign in using an existing account
+                # print(
+                #     "Username or password does not exist, please create an account or sign in using an existing account")
+                # select()  # run select
+                returnArr = [
+                    1, "Username or password does not exist, please create an account or sign in using an existing account"]
+        except:
+            # let the user know that the username or password does not existm and they shouuld either create an account or sign in using an existing account
+            #     "Username or password does not exist, please create an account or sign in using an existing account")
+            # select()  # run select
+            returnArr = [
+                1, "Username or password does not exist, please create an account or sign in using an existing account"]
+    else:  # if the user does not input an option
+        # let the user know that they need to choose one of the options listed
+        # print("Please choose one of the following options")
+        # login()  # run login
+        returnArr = [0, "please enter user and password"]
+    database.close()  # close database text file
+    return returnArr
+
+
+def delete(username, password):
+    database = open("Database", "r")  # open and read the database text file
+    # create a variable line and set it to read the lines in the database text file
+    confirm = ""
+    line = database.readlines()
+    for i in range(len(line)):  # for i in the range of the length of line
+        # if the username and password entered is equal to line then
+        if (username + ", " + password + "\n") == line[i]:
+            # remove the line which will delete that account
+            line.remove(line[i])
+            # let the user name the account is deleted
+            confirm = "Successfully deleted! please create a new account, or login"
+            break
+    # open and write in the database text file
+    database = open("Database", "w")
+    for i in line:  # for i in line
+        database.write(i)  # write it in the text file
+    database.close()  # close the text file
+    if (confirm == ""):
+        return "user account not found"
+    else:
+        return confirm
+
+
+# def select():
+#     # asks user for what they want to do either login, signup, or delete an account
+#     option = input("Login | Signup | Delete:")
+#     if option.casefold() == "Signup".casefold():  # if user chooses signup
+#         signup()  # run signup
+#     elif option.casefold() == "Login".casefold():  # if user chooses login
+#         login()  # run login
+#     elif option.casefold() == "Delete".casefold():  # if user chooses delete
+#         delete()  # run delete
+#     else:
+#         # if user doesn't seleect anything, tell them to select something
+#         print("Please select an option")
+#         select()  # run select
