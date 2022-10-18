a34809f9b2af19a4a727b1f8f65c445717de4657
Samarth Mehta <mehtas30@mcmaster.ca>
2022-10-18 18:42:20 -0400

    SQL added and extra gui features

--- DCM/GUI.py
+++ DCM/GUI.py
@@ -1,4 +1,6 @@
 # from cgitb import text
+from email.policy import EmailPolicy
+import enum
 import tkinter as tk
 from tkinter import messagebox
 from options import *
@@ -63,26 +65,111 @@ class modeP(tk.Frame):
         tk.Frame.__init__(self, parent, bg=BGCOLOR)
         self.controller = controller
 
+        LRLStringVar = tk.StringVar()
+        URLStringVar = tk.StringVar()
+        AampStringVar = tk.StringVar()
+        APWStringVar = tk.StringVar()
+        VampStringVar = tk.StringVar()
+        VPWStringVar = tk.StringVar()
+        ARPStringVar = tk.StringVar()
+        VRPStringVar = tk.StringVar()
+
         def displayParam(userName, mode):
-            readButt.destroy()
-            values = readMethod(userName, mode)
-            if (values == []):
-                messagebox.showinfo(message="Parameters have no value")
+            readButt.grid_remove()
+            setButt.grid_remove()
+            if (mode == "AOO"):
+                parameterList = getParams(userName, mode)
+                if (parameterList == None):
+                    parameterlabel.config(text="no parameters")
+                else:
+
+                    # get from text file
+                    pLabelText = "LRL: " + \
+                        str(parameterList[0])+"\n URL: "+str(parameterList[1])+"\n AAMP: " + \
+                        str(parameterList[2])+"\n APW: " + \
+                        str(parameterList[3])
+                    parameterlabel.config(text=pLabelText)
+            elif (mode == ""):
+                parameterlabel.config(text="")
             else:
-                tkValues = []
-                for i in len(values):
-                    tkValues[i] = tk.StringVar()
-                    tkValues[i].set(values[i])
+                parameterlabel.config(text="diff mode")
+
+            parameterlabel.grid(row=2, column=1)
+
+        def setParam(mode):
+            readButt.grid_remove()
+            setButt.grid_remove()
+            if (mode == 'AOO'):
+                paramEntries[0].config(textvariable=LRLStringVar)
+                paramEntries[1].config(textvariable=URLStringVar)
+                paramEntries[2].config(textvariable=AampStringVar)
+                paramEntries[3].config(textvariable=APWStringVar)
+                for i in range(4):
+                    paramInstructions[i].grid(row=i+1, column=0, pady=5)
+                    paramEntries[i].grid(row=i+1, column=1, pady=5)
+            submit.grid(row=5, column=1, pady=5)
+
+        def verifySubmit(user, mode):
+            if (mode == "AOO"):
+                # verify values code
+                paramList = []
+                print(LRLStringVar.get()+" LRL should be 1")
+                paramList.append(LRLStringVar.get())
+                paramList.append(URLStringVar.get())
+                paramList.append(AampStringVar.get())
+                paramList.append(APWStringVar.get())
+            setParams(user, paramList, mode)
+            LRLStringVar.set("")
+            URLStringVar.set("")
+            AampStringVar.set("")
+            APWStringVar.set("")
+            VampStringVar.set("")
+            VPWStringVar.set("")
+            ARPStringVar.set("")
+            VRPStringVar.set("")
+            messagebox.showinfo(message="Success")
+
+        def backPressed():
+            parameterlabel.config(text="")
+            parameterlabel.grid_forget()
+            readButt.grid(row=1, column=1, pady=5)
+            setButt.grid(row=2, column=1, pady=5)
+            submit.grid_forget()
+            for i, p in enumerate(paramArr):
+                paramEntries[i].config(textvariable=emptyStringVar)
+                paramInstructions[i].grid_forget()
+                paramEntries[i].grid_forget()
+
+            controller.dispFrame("afterLogin")
+
+        # widgets
+        parameterlabel = tk.Label(self, text="", fg='#F2BA49', bg=BGCOLOR,
+                                  justify='center', font="default, 25")
+
+        paramArr = ['LRL', 'URL', 'Aamp', 'APW', 'VAMP', 'VPW', 'VRP', 'ARP']
+        paramEntries = []
+        paramInstructions = []
+        emptyStringVar = tk.StringVar(self, "")
+        submit = tk.Button(self, text="Submit",
+                           width=20, height=2, command=lambda: verifySubmit(controller.sharedUser["username"].get(), controller.sharedUser["mode"].get()))
+        for index, param in enumerate(paramArr):
+            paramInstructions.append(tk.Label(self, text=paramArr[index], fg='#F2BA49', bg=BGCOLOR,
+                                              justify='center', font="default, 25"))
+            paramEntries.append(tk.Entry(self, bg="#FFFF9F", fg=BGCOLOR,
+                                         textvariable=emptyStringVar, width=20))
 
         title = tk.Label(self, textvariable=controller.sharedUser["mode"], fg='#F2BA49', bg=BGCOLOR,
                          justify='center', font="default, 25")
         title.grid(row=0, column=1, pady=10, padx=200)
-        welcomeButt = tk.Button(self, text="Back",
-                                width=5, height=2, command=lambda: controller.dispFrame("afterLogin"))
-        welcomeButt.grid(row=0, column=0, pady=5)
+        backButt = tk.Button(self, text="Back",
+                             width=5, height=2, command=lambda: backPressed())
+        backButt.grid(row=0, column=0, pady=5)
         readButt = tk.Button(self, text="Read Parameters",
                              width=20, height=2, command=lambda: displayParam(controller.sharedUser["username"].get(), controller.sharedUser["mode"].get()))
         readButt.grid(row=1, column=1, pady=5)
+        setButt = tk.Button(self, text="Set Parameters",
+                                       width=20, height=2, command=lambda: setParam(controller.sharedUser["mode"].get()))
+        setButt.grid(row=2, column=1, pady=5)
 
 
 class afterLogin(tk.Frame):
@@ -105,6 +192,11 @@ class afterLogin(tk.Frame):
             connected = True  # testing purposes
             connection.set("Connected" if connected else "Disconnected")
 
+        def signOut():
+            username.config(
+                textvariable=controller.sharedUser["username"].set(""))
+            controller.dispFrame("welcomeP")
+
         connection = tk.StringVar(
             self, "Connected" if connected else "Disconnected")
         showConnection = tk.Label(self, textvariable=connection, fg='Blue', bg=BGCOLOR,
@@ -120,9 +212,9 @@ class afterLogin(tk.Frame):
                             justify='center', font="default, 25")
         username.grid(row=1, column=2, padx=150)
         # back button
-        welcomeButt = tk.Button(self, text="To Main",
-                                width=5, height=2, command=lambda: controller.dispFrame("welcomeP"))
-        welcomeButt.grid(row=0, column=0, pady=5)
+        soButt = tk.Button(self, text="Sign Out",
+                           width=5, height=2, command=lambda: signOut())
+        soButt.grid(row=0, column=0, pady=5)
         AOO = tk.Button(self, text="AOO", width=5, height=2,
                         command=lambda: modeSelect("AOO"))
         AOO.grid(row=1, column=0, pady=5)
@@ -150,22 +242,28 @@ class loginP(tk.Frame):
             loginInfo = login(user, password)
             logCode = loginInfo[0]
             logMsg = loginInfo[1]
-            messagebox.showinfo(message=logMsg)
-            if (logCode == 2):
+            if (logCode == 2):  # success
+                enterPass.set("")
                 controller.dispFrame("afterLogin")
-            elif (logCode == 1):
+            elif (logCode == 1):  # Issue type 1
+                messagebox.showinfo(message=logMsg)
                 clearBox()
                 controller.dispFrame("welcomeP")
-            else:
+            else:  # Issue type 0 or unknown
+                messagebox.showinfo(message=logMsg)
                 clearBox()
+
+        def signOut():
+            clearBox()
+            controller.dispFrame("welcomeP")
         # Title label
         title = tk.Label(self, text='Login', fg='#F2BA49', bg=BGCOLOR,
                          justify='center', font="default, 25")
         title.grid(row=0, column=1, pady=10, padx=200)
         # button for option select on welcome screen
         welcomeButt = tk.Button(self, text="To Main",
-                                width=5, height=2, command=lambda: controller.dispFrame("welcomeP"))
-        #enterUser = tk.StringVar()
+                                width=5, height=2, command=lambda: signOut())
+        # enterUser = tk.StringVar()
         enterPass = tk.StringVar()
         user = tk.Entry(self, bg="#FFFF9F", fg=BGCOLOR,
                         textvariable=controller.sharedUser["username"], width=20)
--- DCM/storeAttributes.py
+++ DCM/storeAttributes.py
@@ -1,25 +1,130 @@
-def readMethod(user, mode):
+import sqlite3
+
+
+def createDB():
+    dbConnection = sqlite3.connect('parameter.db')
+    curs = dbConnection.cursor()
+    try:
+        # makes text file parameter.db creates if doesnt exist
+        curs.execute('''CREATE TABLE userParams(
+            USER text PRIMARY KEY NOT NULL,
+            LRL real,
+            URL real,
+            AAMP real,
+            APW real,
+            VAMP real,
+            VPW real,
+            VRP real,
+            ARP real
+         )''')  # docstring comment for multiple lines
+        dbConnection.commit()
+        curs.close()
+    except:
+        print("already created parameter db\n")
+
+
+def updateUser(username):
+    dbConnection = sqlite3.connect('parameter.db')
+    curs = dbConnection.cursor()
+    curs.execute('INSERT INTO userParams VALUES (:user,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL)', {
+                 'user': username})
+    # data = curs.execute('SELECT * From userParams')
+    # print('create \n')
+    # print(data.fetchall())  # testing
+    dbConnection.commit()
+    curs.close()
+
+
+def paramDelete(username):
+    dbConnection = sqlite3.connect('parameter.db')
+    curs = dbConnection.cursor()
+    curs.execute('DELETE from userParams WHERE USER = :user',
+                 {'user': username})
+    # data = curs.execute('SELECT * From userParams')
+    # print("delete \n") TESTING
+    # print(data.fetchall())  # testing
+    dbConnection.commit()
+    curs.close()
+
+
+def setParams(username, pChange, mode):
+    dbConnection = sqlite3.connect('parameter.db')
+    curs = dbConnection.cursor()
+    if (mode == "AOO"):
+        curs.execute('UPDATE userParams set LRL= :lrl, URL= :url, AAMP= :aamp, APW= :apw WHERE USER= :user', {
+                     'user': username, 'lrl': pChange[0], 'url': pChange[1], 'aamp': pChange[2], 'apw': pChange[3]})
+    elif (mode == "VOO"):
+        curs.execute('UPDATE userParams set LRL= :lrl, URL= :url, VAMP= :vamp, VPW= :vpw WHERE USER= :user', {
+                     'user': username, 'lrl': pChange[0], 'url': pChange[1], 'vamp': pChange[2], 'vpw': pChange[3]})
+    elif (mode == "AAI"):
+        curs.execute('UPDATE userParams set LRL= :lrl, URL= :url, AAMP= :aamp, APW= :apw, ARP= :arp WHERE USER= :user', {
+            'user': username, 'lrl': pChange[0], 'url': pChange[1], 'aamp': pChange[2], 'apw': pChange[3], 'arp': pChange[4]})
+    elif (mode == "VVI"):
+        curs.execute('UPDATE userParams set LRL= :lrl, URL= :url, VAMP= :vamp, VPW= :vpw, VRP= :vrp WHERE USER= :user', {
+            'user': username, 'lrl': pChange[0], 'url': pChange[1], 'vamp': pChange[2], 'vpw': pChange[3], 'vrp': pChange[4]})
+    else:
+        print('mode doesnt exist')
+    # data = curs.execute('SELECT * From userParams')
+    # print("setting \n")  # TESTING
+    # print(data.fetchall())  # testing
+    dbConnection.commit()
+    curs.close()
+
+
+def getParams(username, mode):
+    dbConnection = sqlite3.connect('parameter.db')
+    curs = dbConnection.cursor()
     params = []
-    paramDb = open("ParamDb", 'r')
-    lines = paramDb.readlines()
-    for i in range(len(lines)):
-        nameSplit = lines[i].split(":")
-        name = nameSplit[1].strip()
-        print(name+"ns1")
-        if (name == user):
-            print("first if done")
-            modeFound = 0
-            i += 1
-            while (modeFound == 0):
-                modeSplit = lines[i].split(":")
-                modeSplit[0].strip()
-                print(modeSplit)
-                if (modeSplit[0] == mode):
-                    print("found")
-                    modeFound = 1
-                else:
-                    i += 1
-            params = modeSplit[1].strip().split(",")
-            print("params"+params[0]+" "+params[8])
-    paramDb.close()
-    return params
+    if (mode == "AOO"):
+        params = curs.execute('SELECT LRL,URL,AAMP,APW FROM userParams WHERE USER= :user', {
+            'user': username})
+    elif (mode == "VOO"):
+        params = curs.execute('SELECT LRL,URL,VAMP,VPW FROM userParams WHERE USER= :user', {
+            'user': username})
+    elif (mode == "AAI"):
+        params = curs.execute('SELECT LRL,URL,AAMP,APW,ARP FROM userParams WHERE USER= :user', {
+            'user': username})
+    elif (mode == "VVI"):
+        params = curs.execute('SELECT LRL,URL,VAMP,VPW,VRP FROM userParams WHERE USER= :user', {
+            'user': username})
+    else:
+        print('mode doesnt exist')
+        return []
+
+    # print(params.fetchone())
+    magic = params.fetchone()  # stack pop
+    paramsRow = []
+    if (magic != None):
+        print("entered")
+        for element in magic:
+            paramsRow.append(element)
+        curs.close()
+        return paramsRow
+    else:
+        curs.close()
+        print("empty")
+        return None
+
+
+# testing
+# createDB()
+# updateUser('test')
+# setParams('bobby', [1, 2, 3, 4, 5], 'AOO')
+# hi = getParams('test', 'AOO')
+# print("this is what gets returned")
+# print(hi)
+
+# paramDelete('bobby')
+
+# class AOOClass:
+#     def __init__(self, user):
+#         self.param = []
+#         self.user = user
+
+#     def getParam(self):
+#         AOOTxt = open("AOO", "a+")
+#         AOOLines = AOOTxt.readlines()
+#         AOOLines = map(lambda x: x.strip(), AOOLines)
+#         if (AOOLines == []):
+#             return []
+#         AOOTxt.close()
