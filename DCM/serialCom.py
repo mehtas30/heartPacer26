f210e577258395dd590737c7ae47e72b8b3fed7e
Burhanuddin Qadir <qadirb@mcmaster.ca>
2022-11-24 15:35:56 -0500

    Updated serial communication with provided TA code

--- DCM/serialCom.py
+++ DCM/serialCom.py
@@ -1,4 +1,4 @@
-# import serial
+import serial
 
 # ser = serial.Serial('COM5', 115200, timeout=10)
 
@@ -11,3 +11,42 @@ else:
     different = True
 
 # if connected change- reload gui with tk.after
+#Serial communication baud rate shall be 57600
+
+import serial
+import serial.tools.list_ports
+import struct
+
+# Mac ports, for windows you have to find the ports yourself
+frdm_port = "/dev/cu.usbmodem0000001234561"
+
+Start = b'\x16'
+SYNC = b'\x22'
+Fn_set = b'\x55'
+red_en = struct.pack("B", 1)
+green_en = struct.pack("B", 0)
+blue_en = struct.pack("B", 1)
+off_time = struct.pack("f", 3.1415926)
+switch_time = struct.pack("H", 500)
+
+Signal_set = Start + Fn_set + red_en + green_en + blue_en + off_time + switch_time
+Signal_echo = Start + SYNC + red_en + green_en + blue_en + off_time + switch_time
+
+with serial.Serial(frdm_port, 115200) as pacemaker:
+    pacemaker.write(Signal_set)
+
+with serial.Serial(frdm_port, 115200) as pacemaker:
+    pacemaker.write(Signal_echo)
+    data = pacemaker.read(9)
+    red_rev = data[0]
+    green_rev = data[1]
+    blue_rev = data[2]
+    off_rev =  struct.unpack("f", data[3:7])[0]
+    switch_rev =  struct.unpack("H", data[7:9])[0]
+
+print("From the board:")
+print("red_en = ", red_rev)
+print("green_en = ", green_rev)
+print("blue_en = ", blue_rev)
+print("off_time = ",  off_rev)
+print("switch_time = ", switch_rev)
